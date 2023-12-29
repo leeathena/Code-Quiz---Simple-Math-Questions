1dc0140ebd143e318a1637aa79208681c03f9dd8
athena lee <hoyan.la@gmail.com>
2023-12-30 03:03:19 +0800

    Questions, answers, and next questions logic added

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,150 @@
+// Set of questions --> array of objects
+const timer = document.getElementById("time")
+const startButton = document.getElementById("start")
+const questions = document.getElementById("questions")
+const questionTitles = document.getElementById("question-title")
+const questionAnswers = document.getElementById("choices")
+const finalScore = document.getElementById("final-score")
+const submitButton = document.getElementById("submit")
+const Winner = document.getElementById("initials")
+
+
+
+// Each question needs the following:
+  // Question text
+  // Set of answers
+  // Which answer is correct
+
+  const questions = [
+    {
+        questions: "What is 2+4?",
+        answers: [
+            {text: "6", correct :true},
+            {text: "8", correct :false},
+            {text: "10", correct :false},
+            {text: "4", correct :false},
+        ]
+},
+{
+    questions: "What is 2X4?",
+    answers: [
+        {text: "6", correct :false},
+        {text: "8", correct :true},
+        {text: "10", correct :false},
+        {text: "4", correct :false},
+    ]
+},
+{
+    questions: "What is 4 divided by 2?",
+    answers: [
+        {text: "6", correct :false},
+        {text: "8", correct :false},
+        {text: "2", correct :true},
+        {text: "4", correct :false},
+    ]
+},
+{
+    questions: "What is 4 minus 2?",
+    answers: [
+        {text: "6", correct :false},
+        {text: "2", correct :true},
+        {text: "8", correct :false},
+        {text: "4", correct :false},
+    ]
+},
+{
+    questions: "What is 4 times 2?",
+    answers: [
+        {text: "6", correct :false},
+        {text: "8", correct :true},
+        {text: "2", correct :false},
+        {text: "4", correct :false},
+    ]
+},
+
+// Landing page:
+  // Explanation of the quiz
+  // Start button
+
+
+// Click the start button:
+  // Landing page goes away
+  // Timer starts
+  // The first question appears (with its answers)
+
+fuction startQuiz(e){
+    currentQuestionIndex = 0;
+    timer = 60000;
+    startButton = e.target;
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNum = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNum + "." +currentQuestion.question;
+
+    currentQuestions.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        questionAnswers.appendChild(button);
+        if (answer,correct){
+            button.dataset,correct = answer.correct;
+        }
+        button.addEventListener ("click", selectAnswer);
+
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selctedBtn = e.target;
+    const result = getElementById("result")
+    const isCorrect = selctedBtn.dataset.correct ==="true";
+    if(isCorrect){
+        selectedBtn.classList.add ("correct");
+        result.textContent = "Correct!"
+    }else{
+        selectedBtn.classList.add ("incorrect");
+        result.textContent = "Incorrect!"
+        timer = timer - 100;
+    } 
+    setTimeout(function() {
+        let nextQuestionNum = currentQuestionIndex + 1;
+        let nextElem = document.getElementById('question' + nextQuestionNum);
+        if (nextElem) {
+            nextElem.style.display = 'block';
+        } else {
+            alert("End of Quiz");
+        }
+    }, 3000);
+}
+
+
+// For each question:
+  // User clicks an answer
+  // Their choice is compared to the correct answer as stored in the question's object
+  // If correct, tell them
+  // If incorrect, tell them AND subtract time from the timer
+  // Optional: play a sound for correct or incorrect
+  // Either way, the question disappears after a few seconds and the next question appears
+
+// After the last question:
+  // Timer stops
+  // Question disappears
+  // Form appears for user to enter their initials
+  // Display their score
+
+// User submits form
+  // Initials and score get stored in local storage
+  // User is taken to the high scores page
+  // High scores are listed, sorted highest to lowest
+  // User has option to take the quiz again
